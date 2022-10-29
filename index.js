d2a6c5e62431b72d0cf10b091b8e696f664f02fa
kali <pk90264180@gmail.com>
2022-10-29 09:40:10 +0530

    updated socket code

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const hbs = require('hbs');
 const app = express();
+const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
 require('./conn');
@@ -10,24 +12,24 @@ app.use(cors());
 app.use(express.json());
 dotenv.config({ path: './.env' });
 const PORT = process.env.PORT;
-
-const io = new Server({
+const server = http.createServer(app);
+app.set('view engine', 'ejs');
+const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
+    origin: '*',
   },
 });
-
 io.on('connection', (socket) => {
-  // console.log(`user connected to ${socket.id}`);
   socket.on('send_message', (data) => {
     console.log(data.message);
+    const chat = data.message;
+    io.emit('send_message', chat);
   });
 });
-app.get('/', (req, res) => {
-  res.send('hello this is site I am running');
-});
 
+app.get('/', (req, res) => {
+  res.render('index');
+});
 app.get('/api/item', (req, res) => {
   db.collection('orders')
     .find()
@@ -37,7 +39,6 @@ app.get('/api/item', (req, res) => {
       res.send(docs);
     });
 });
-app.get('/api/item/:id', (req, res) => {});
 app.post('/api/item', async (req, res) => {
   const {
     order_id,
@@ -68,6 +69,6 @@ app.post('/api/item', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`site is live at http://localhost:${PORT} `);
+server.listen(PORT, () => {
+  console.log(`site is live on http://localhost:${PORT}`);
 });
